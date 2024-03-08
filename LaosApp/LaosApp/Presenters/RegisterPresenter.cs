7ae578f7ccd6d4034c8f020328894c60d63d359f
SoporteDecome <soporte@decome.com.mx>
2024-03-08 00:25:00 -0600

    add files of register

---
+++ LaosApp/LaosApp/Presenters/RegisterPresenter.cs
@@ -0,0 +1,22 @@
+﻿using LaosApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaosApp.Presenters 
+{
+    internal class RegisterPresenter : IRegisterPresenter
+    {
+        private readonly IRegisterDataService _registerDataService;
+
+
+
+        public RegisterPresenter(IRegisterDataService registerDataService)
+        {
+
+            _registerDataService = registerDataService;
+        }
+    }
+}
---
+++ LaosApp/LaosApp/Views/RegisterView/RegisterPage.xaml.cs
@@ -0,0 +1,11 @@
+using LaosApp.Interfaces;
+
+namespace LaosApp.Views.RegisterView;
+
+public partial class RegisterPage : ContentPage , IRegisterView
+{
+	public RegisterPage()
+	{
+		InitializeComponent();
+	}
+}
